d6a6c3f7a3f56e7692883205ff9b13c281595b86
Aman Jain <aman2.jain@paytm.com>
2020-06-29 00:58:30 +0530

    UIColor Picker iOS 14

--- ColorPicker/ViewController.swift
+++ ColorPicker/ViewController.swift
@@ -8,12 +8,45 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    private var pickedColor = UIColor.systemTeal
+    private var colorPicker = UIColorPickerViewController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        colorPicker.delegate = self
+        setupBarButton()
+        view.backgroundColor = pickedColor
     }
-
-
+    
+    @IBAction func didTapColorPicker(_ sender: Any) {
+        selectColor()
+    }
+    
+    private func selectColor() {
+        colorPicker.supportsAlpha = true
+        colorPicker.selectedColor = pickedColor
+        self.present(colorPicker, animated: true)
+    }
+    
+    private func setupBarButton() {
+        let pickColorAction = UIAction(title: "Pick Color") { _ in
+            self.selectColor()
+        }
+        let pickColorBarButton = UIBarButtonItem(image: UIImage(systemName: "eyedropper"), primaryAction: pickColorAction)
+        navigationItem.rightBarButtonItem = pickColorBarButton
+    }
 }
 
+
+extension ViewController : UIColorPickerViewControllerDelegate {
+    
+    func colorPickerViewControllerDidSelectColor(_ viewController: UIColorPickerViewController) {
+        pickedColor = viewController.selectedColor
+        view.backgroundColor = pickedColor
+    }
+    
+    func colorPickerViewControllerDidFinish(_ viewController: UIColorPickerViewController) {
+        print("Color Picker Controller Did Finish")
+    }
+}
